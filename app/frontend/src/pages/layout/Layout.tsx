d9e3cdff1b0cce0d9004106338b33dc00258c47f
riwata66 <69414721+riwata66@users.noreply.github.com>
2024-01-31 05:44:21 +0000

    文言変更

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -20,7 +20,7 @@ const Layout = () => {
                         <ul className={styles.headerNavList}>
                             <li>
                                 <NavLink to="/" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
-                                    総合受付
+                                    総合
                                 </NavLink>
                             </li>
                             <li className={styles.headerNavLeftMargin}>
